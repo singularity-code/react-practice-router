ae7a4b7d9122df54a08ea7630d525062d2239de9
DL <neoxrock@gmail.com>
2021-11-02 10:23:21 +1100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span><Link to="/">Main</Link></span>
+      <span><Link to="/about">About</Link></span>
+      <span><Link to="/info">Info</Link></span>
+      <Route path="/" component={Main} exact={true}/>
+      <Route path={["/about", "/info"]} component={About} />
     </div>
   );
 }
