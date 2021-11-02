369cab6a03701010a3f4084a6fdb639f150459f5
DL <neoxrock@gmail.com>
2021-11-02 14:23:17 +1100

    NavLink

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, NavLink } from "react-router-dom";
 import Profile from "./Profile";
 import "./Profiles.scss";
 
 const Profiles = () => {
+  const activeStyle = {
+    background: "#ff00ff",
+    color: 'white',
+  };
   return (
     <div>
       <span>
         <Link to="/profiles/id1">User1</Link>
       </span>
       <span>
-        <Link to="/profiles/id2">User2</Link>
+        <NavLink activeStyle={activeStyle} to="/profiles/id2">User2</NavLink>
       </span>
       <Route path="/profiles" exact render={() => <div>Select a user</div>} />
       <Route path="/profiles/:username" component={Profile} />
